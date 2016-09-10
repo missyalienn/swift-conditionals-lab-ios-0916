332cf9a8b020d69a559f9b6bdfe7c5f50fea8691
Missy Allan <missyallan@Melissas-Air.fios-router.home>
2016-09-10 13:21:17 -0400

    conditionals lab complete

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -24,53 +24,87 @@ let b = 32
 /*: Question 1
 ### 1. Print the result of a greater than or equal to b
 */
-// write your code here
 
+ var result1 =  a >= b
+ print(result1)
 
 /*: Question 2
 ### 2. Print the result of a modulo b is equal to zero
 */
-// write your code here
+
+var result2 = a % b == 0
+print(result2)
+
 
 
 /*: Question 3
 ### 3. Print the result of y times b less than or equal to a
 */
-// write your code here
+
+ var result3 = y * b <= a
+ print(result3)
 
 
 /*: Question 4
 ### 4. Print the inverse of a greater than or equal to b
 */
-// write your code here
+
+
+var result4 = !(a >= b)
 
 
 
 /*: Question 5
 ### 5. Print "true" if a modulo b is equal to zero
 */
-// write your code here
+
+var result5 =  a % b == 0
 
 
 /*: Question 6
 ### 6. Print "true" if a divided by b is greater than x
 */
-// write your code here
+
+
+let result6 = (a/b)
+    if result6 > Int(x) {
+        print("True")
+    }else{
+        print("Not true")
+    }
 
 
 
 /*: Question 7
 ### 7. Print "true" if y divided by x is greater than three, otherwise print false
 */
-// write your code here
 
+
+let result7 = (y/Int(x))
+
+if result7 > 3 {
+    print("True")
+
+}else{
+    print("False")
+
+}
 
 
 
 /*: Question 8
 ### 8. Print "true" if y is greater than x and a divided by b is greater than 9
 */
-// write your code here
+
+let result8 = (y > Int(x))
+let result9 = (a/b)
+
+
+if (result8 : true) && result9 > 9 {
+    print("True")
+}
+
+
 
 
 
@@ -78,30 +112,52 @@ let b = 32
 /*: Question 9
 ### 9. Write a function "isGreater" that takes two Int arguments and returns true if the first is greater than the second and false if they're not
 */
-// write your code here
 
 
+func isGreater(param1:Int, param2:Int) -> Bool {
+    if (param1 > param2) {
+        return true
+    }else{
+     return false
+}
+}
 
+
+isGreater(10, param2:9)
 
 
 
 /*: Question 10
 ### 10. Write a function "isForceWith" that takes a String argument and returns true if the argument is the name of someone with whom the force is strong, and otherwise returns false. People who have the force are Luke, Leia, Anakin, Obi Wan, Yoda, Vader.
 */
-// write your code here
 
-
+func isForceWith(name:String) -> Bool {
+    if name == "Luke" {
+        return true
+    } else if name == "Leia" {
+        return true
+    } else if name == "Anakin" {
+        return true
+    } else if name == "Obi Wan" {
+        return true
+    } else if name == "Yoda" {
+        return true
+    } else if name == "Vader" {
+        return true
+    } else {
+        return false
+}
+}
 
 /*: Question 11
 ### 11. Create a function where the two arguments represent different bank account (one of those bank accounts is yours). What type should these arguments be if we are to then perform some math operations on them? Setup a conditional that will add 10 to your funds and minus 10 from the other funds (bank account) if the other account won't go negative if we were to take 10 dollars from it.
 */
-// write your code here
 
+func bankAccount( a : Double, b: Double){
+        if (b-10>=0){
+                 a + 10
+                     b - 10
+           }
+}
+bankAccount(5000, b:321)
 
-
-
-
-/*:
- Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
- */
-// ❤️
